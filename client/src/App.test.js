dbc257be9694adc767b27119a34c72e433cc3f0a
KerolisKhalaf <khalafshief@gmail.com>
2026-06-14 20:31:59 +0300

    refactor the workflows by moc jest axios

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import axios from 'axios';
+
+// 1. نقوم بعمل محاكاة لمكتبة axios
+jest.mock('axios');
 
 test('renders learn react link', () => {
+  // 2. نجعل axios يرد بقيمة وهمية ناجحة بدلاً من محاولة الاتصال بالخادم
+  axios.get.mockResolvedValue({ data: [] });
+
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
